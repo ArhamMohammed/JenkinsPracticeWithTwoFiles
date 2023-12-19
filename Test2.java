ebbf125579dbf746324e4d5dec89892d84576102
Mir Mohammed Arham <send2arhamm@gmail.com>
2023-12-19 12:01:27 +0530

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -1,5 +1,7 @@
 class Test{
   public static void main(String []args){
-    System.out.println("Welcome to Jenkins")
+    System.out.println("Welcome to Jenkins");
+    System.out.println("Welcome to Jenkins");
+    System.out.println("Welcome to Jenkins");
   }
 }
